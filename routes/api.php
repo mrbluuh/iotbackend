67d7753859588c503b5180e3fe0676b917cd3621
MrBluuH <salazarvl46@gmail.com>
2018-05-31 22:16:15 -0500

    Cors

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
 
 Route::post('login', 'Api\Auth\LoginController@login');
 Route::post('refresh', 'Api\Auth\LoginController@refresh');
